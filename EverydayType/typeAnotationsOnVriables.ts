764b3ae881309b161edb5ba6ebd546daf3290333
beboantonio <beboantonio1@hotmail.com>
2021-08-30 08:07:02 -0300

    add type annotation on variables

---
+++ EverydayType/typeAnotationsOnVriables.ts
@@ -0,0 +1,11 @@
+// when you declare a variable using CONST, VAR or LET you can optionally
+// add a type annotation to explicitly specify the type of the variable:
+
+let anyName : string = "Alice";
+const anySecondName : string = "Antonio";
+var anyThirdName: string = "Peter";
+
+// No type annotation needed -- 'myName' inferred as type 'string'
+let myName = "Alice";
+
+
