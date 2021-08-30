3560a796bd69536095e6ec2a69e5ba507f82ec31
beboantonio <beboantonio1@hotmail.com>
2021-08-30 20:46:51 -0300

    play with anonymous functions

--- EverydayType/anonymousFunction.ts
+++ EverydayType/anonymousFunction.ts
@@ -5,12 +5,12 @@ let names = ["Alice", "Bob", "Eve"];
 names.forEach( function (s) 
 {
     // this code throws has error
-    console.log(s.toUppercase()); 
+    console.log(s.toUpperCase()); 
 })
 
 // contextual typing also applies to arrow (=>) functions
 names.forEach( (s) => 
 {
     // this code throws an error
-    console.log(s.toLowercase()); 
+    console.log(s.toLowerCase()); 
 })
