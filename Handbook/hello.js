e55bf3e1864a1e9ec559f16726c44c622bdc1fcf
beboantonio <beboantonio1@hotmail.com>
2021-08-30 07:17:16 -0300

    first commit

---
+++ Handbook/hello.js
@@ -0,0 +1,5 @@
+// this is an indutrial-grade general-purpose greeter
+function greet(person, date) {
+    console.log(`hello ${person}, i am ${date.toDateString()}`);
+}
+greet("Medisson", new Date());
